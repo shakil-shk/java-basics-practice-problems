e9a8a521ab95e6197358aa570d014c9df3095260
shakil-ahammed-cse <shakil.ahammed.pro@gmail.com>
2025-12-23 23:39:18 +0600

    Math Code Added

---
+++ Mathematics/Check_Prime.java
@@ -0,0 +1,34 @@
+package shk.practice.Mathematics;
+import java.util.Scanner;
+
+public class Check_Prime {
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        if (num == 2) {
+            return true;
+        }
+        if (num % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i * i <= num; i += 2) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (isPrime(number)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        scanner.close();
+    }
+}
