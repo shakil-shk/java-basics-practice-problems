89a45342e791fbccb361320ae01c791eb78c3fa7
shakil-ahammed-cse <shakil.ahammed.pro@gmail.com>
2025-12-29 23:13:36 +0600

    New code Added

---
+++ Arrays/Remove_Duplicates.java
@@ -0,0 +1,31 @@
+package shk.practice.Arrays;
+
+public class Remove_Duplicates {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int i = 1;
+
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[j - 1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 0, 1, 1, 1, 2, 2, 3, 3, 4};
+
+        int k = removeDuplicates(nums);
+
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
+
